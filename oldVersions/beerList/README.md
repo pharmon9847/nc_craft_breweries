c33de836ffd1e7c7670c1b99b953948d6f413912
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2020-04-22 22:42:29 -0400

    commit new code

--- oldVersions/beerList/README.md
+++
@@ -1,3 +0,0 @@
-# north_carolina_breweries
-
-# Project 2 with UNC Coding Bootcamp
--- oldVersions/beermap/assets/js/config.js
+++
@@ -1,1 +0,0 @@
-const API_KEY = "pk.eyJ1IjoiY21ib29uZTIwMDUiLCJhIjoiY2s3d2hqNTJwMDIybDNqbG5vaHhpbTV6biJ9.CXopDAMC8clWafhdOuxlPw";
